a84e983092a7469d0ad1bd900ca4f7f73f08037c
Nathan Naze <nanaze@gmail.com>
2014-04-12 23:44:50 -0400

    Run the scripts in Rhino.

--- src/main/java/com/github/nanaze/jscomprun/JsCompRunner.java
+++ src/main/java/com/github/nanaze/jscomprun/JsCompRunner.java
@@ -1,5 +1,7 @@
 package com.github.nanaze.jscomprun;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +11,12 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.Argument;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 import com.google.javascript.jscomp.ErrorManager;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.javascript.jscomp.JSModule;
 import com.google.javascript.jscomp.JSModuleGraph;
@@ -50,7 +55,10 @@ class JsCompRunner {
 		for (String path : flags.sourcePaths) {
 			ErrorManager errorManager = new LoggerErrorManager(logger);
 			JsFileParser jsFileParser = new JsFileParser(errorManager);
+			jsFileParser.setIncludeGoogBase(true);
+			
 			DependencyInfo dependencyInfo = jsFileParser.parseFile(path, null);
+
 			dependencyInfoList.add(dependencyInfo);
 			Preconditions.checkArgument(jsFileParser.didParseSucceed(),
 					"Parse failed");
@@ -76,8 +84,26 @@ class JsCompRunner {
 		}
 		
 		logger.info("Printing source file paths to stderr.");
+		List<String> scriptPaths = Lists.newArrayList();
 		for (DependencyInfo dependency : sortedDependencies.getSortedDependenciesOf(entryPointInputs)) {
-			System.out.println(dependency.getName());
+			String path = dependency.getName();
+			System.out.println(path);
+			scriptPaths.add(path);
 		}
+		
+		runClosureScripts(scriptPaths);
+	}
+
+	private static void runClosureScripts(List<String> scriptPaths) throws IOException {
+		logger.info("Running scripts in Rhino...");
+		
+		Context context = Context.enter();
+        Scriptable scope = context.initStandardObjects();
+        
+        for (String path : scriptPaths) {
+        	FileReader reader = new FileReader(path);
+        	context.evaluateReader(scope, reader, path, 1, null);
+        	reader.close();
+        }
 	}
 }
